5cb6d7b61913819b8686afd709b273857c183565
Sam <samnadir.nait@outlook.com>
2026-09-23 14:04:41 +0200

    feat : implementing the first version with only the calculations. No user input is checked. We're considering the user entering the corrects values.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
 /* --------------------------- 
 Laboratoire : 02
-Auteur(s) : 
-Date : 
+Auteur(s) : Sam Nait Abbou
+Date : 23.09.2026
 But : Calcul du temps de trajet 
 Remarque(s) : 
 --------------------------- */
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Bienvenue dans sur votre calculateur de distance" << endl;
+    cout << "------------------------------------------------" << endl;
+
+    // Enregistrement des distances robot-objet
+    double dx,dy;
+    cout << "Inscrire la distance dx (m): ";
+    cin >> dx;
+    cout << endl;
+    cout << "Inscrire la distances dy (m): ";
+    cin >> dy;
+    cout << endl;
+
+    // Enregistrement de la distance sur la route avant chemin rocheux
+    double l_1;
+    cout << "Inscrire la distance l_1 (m) : ";
+    cin >> l_1;
+    cout << endl;
+
+    double l_2 = sqrt(pow(dy- l_1,2)+pow(dx,2)); // Calcul de L2 en metre
+
+    // Enregistrement des vitesses
+    double v_rt, v_rx;
+    cout << "Inscrire la vitesse sur route (m/s) : ";
+    cin >> v_rt;
+    cout << endl;
+    cout << "Inscrire la vitesse sur terrain rocheux (m/s) : ";
+    cin >> v_rx;
+    cout << endl;
+
+    double t = (l_1/v_rt) + (l_2/v_rx); // Calcul du temps
+
+    cout << "Temps de parcours total: " << t << " secondes" << endl;
+
+    return 0;
+}
--- pseudo.md
+++ pseudo.md
@@ -1,1 +1,40 @@
-# Pseudo code de votre programme
+# Entrée / Sortie
+
+Entrées :
+
+- Distance robot-objet (`dx`, `dy`)
+- Vitesse du robot sur route et terrain rocheux (`v_rt` ,`v_rx` )
+- La longeur `l1` du premier segment de la route
+
+Sortie :
+
+- temps de trajet `t`
+
+# Sous-problèmes
+
+une seul inconnu manque, la longueur `l2` qui représente le segment sur le terrain rocheux. Pour l’obtenir il faut soustaire la distance `dy` avec L1 et appliquer ensuite le théorème du triangle rectangle avec `dx` .
+
+Soit le calcul suivant:
+
+$$  
+L2 = \sqrt{(dy-L1)^2 + dx^2}  
+$$
+
+Une fois `l2` trouvé on peut trouver le temps `t` total en divisant `l2` par `v_rt` , puis en divisant `l2` par `v_rx` et faire la somme des deux résultats.
+
+Soit le calcul suivant:
+
+$$  
+t = L_1/v_{rt} + L_2/v_{rx}  
+$$
+
+# Pseudo-Code
+
+```cpp
+incription >> dx,dy et l_1
+l_2 = racincarre((dy-l_1)^2 + (dx,2)^2)
+inscription v_rt ,v_rx
+t = (l_1/v_rt) + (v_rx)
+afficher t 
+
+```
